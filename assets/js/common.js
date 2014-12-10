bf61d79035f6cdff299d205803882422a7085306
yoichi kobayashi <info@tplh.net>
2014-12-10 20:55:43 +0900

    汎用関数を追加。

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,22 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2D');
-var fps = 30;
+var ctx = canvas.getContext('2d');
+var fps = 60;
 var frameTime = 1000 / fps;
 
-var toRadian = function(degrees) {
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var getRadian = function(degrees) {
   return degrees * Math.PI / 180;
+};
+
+var getDistance = function(p1x, p1y, p2x, p2y) {
+  var dx = p2x - p1x;
+  var dy = p2y - p1y;
+  return Math.sqrt(dx * dx + dy * dy);
 };
 
 var debounce = function(object, eventType, callback){
--- assets/js/unique.js
+++ assets/js/unique.js
@@ -1,1 +1,62 @@
-var a = 0;
+var animeObjArr = [];
+
+var animeObj = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.frameNum = 0;
+  this.maxFrameNum = 60;
+  this.size = Math.random() * 2;
+  this.color = {
+    r: Math.floor(Math.random() * 33),
+    g: Math.floor(Math.random() * 33),
+    b: Math.floor(Math.random() * 33)
+  }
+};
+
+animeObj.prototype.render = function() {
+  ctx.beginPath();
+};
+
+animeObj.prototype.isLast = function() {
+  if (this.frameNum == this.maxFrameNum) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var render = function() {
+  ctx.clearRect(0, 0, width, height);
+  
+  for (var i = 0; i < animeObjArr.length; i++) {
+    animeObjArr[i].render();
+    animeObjArr[i].frameNum++;
+    if (animeObjArr[i].isLast()) {
+      animeObjArr.splice(i, 1);
+    };
+  };
+  
+  setTimeout(function() {
+    render();
+  }, frameTime);
+};
+
+document.addEventListener('mousemove', function(event) {
+  var x = event.x;
+  var y = event.y;
+
+  //animeObjArr.push(new animeObj(x, y));
+}, false);
+
+document.addEventListener('touchstart', function(event) {
+  event.preventDefault();
+}, false);
+
+document.addEventListener('touchmove', function(event) {
+  var x = event.touches[0].pageX;
+  var y = event.touches[0].pageY;
+
+  //animeObjArr.push(new animeObj(x, y));
+}, false);
+
+render();
