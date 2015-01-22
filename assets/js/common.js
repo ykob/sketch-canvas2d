38af1d7b34bbad65bb3f7475c2dc908bc14da1d1
yoichi kobayashi <info@tplh.net>
2015-01-22 11:53:59 +0900

    getDegree関数を追加。

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,6 +9,10 @@ var getRandomInt = function(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
+var getDegree = function(radian) {
+  return radian / Math.PI * 180;
+};
+
 var getRadian = function(degrees) {
   return degrees * Math.PI / 180;
 };
