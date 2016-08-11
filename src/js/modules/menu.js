f8693e9b55491743294bbabde810a5e760eeae92
yoichi kobayashi <info@tplh.net>
2016-08-11 23:55:38 +0900

    [add] style and animation for menu list.

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,6 +1,7 @@
 export default class Menu {
   constructor() {
     this.$page = $('.c-page');
+    this.$list = $('.c-menu__list');
     this.$btn = $('.c-menu__btn');
     this.setEvent();
   }
@@ -8,9 +9,11 @@ export default class Menu {
     this.$btn.on('click', () => {
       if (this.$page.hasClass('menu-opened')) {
         this.$page.removeClass('menu-opened');
+        this.$list.removeClass('menu-opened');
         this.$btn.removeClass('menu-opened');
       } else {
         this.$page.addClass('menu-opened');
+        this.$list.addClass('menu-opened');
         this.$btn.addClass('menu-opened');
       }
     });
