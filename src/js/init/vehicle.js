052bd28e3beabb411cb4ba668d76a736ac1ca12d
yoichi kobayashi <info@tplh.net>
2016-08-16 23:35:18 +0900

    [WIP] sketch vehicle.

---
+++ src/js/init/vehicle.js
@@ -0,0 +1,14 @@
+import force3 from '../modules/force3.glmatrix.js';
+
+const glMatrix = require('gl-matrix');
+
+export default function() {
+  const vehicles = [];
+
+  const render = () => {
+
+  };
+  const renderLoop = () => {
+
+  };
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import initCommon from './init/common.js'
 import initIndex from './init/index.js'
+import initVehicle from './init/vehicle.js'
 
 const { pathname } = window.location;
 
@@ -8,6 +9,9 @@ const init = () => {
   switch (pathname.replace('index.html', '')) {
     case '/':
       initIndex();
+      break;
+    case '/vehicle.html':
+      initVehicle();
       break;
     default:
   }
