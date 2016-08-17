38f0a0448a4972f39ed38d30b822dafaa2f276f4
yoichi kobayashi <info@tplh.net>
2016-08-17 23:10:27 +0900

    [add] base script of drawing canvas.

--- src/js/init/vehicle.js
+++ src/js/init/vehicle.js
@@ -3,12 +3,22 @@ import force3 from '../modules/force3.glmatrix.js';
 const glMatrix = require('gl-matrix');
 
 export default function() {
+  const canvas = document.getElementById('stage');
+  const ctx = canvas.getContext('2d');
   const vehicles = [];
 
-  const render = () => {
+  let time = 0;
 
+  const render = () => {
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    ctx.arc(window.innerWidth / 2, window.innerHeight / 2, 100, 0, Math.PI * 2, false);
+    ctx.fill();
   };
   const renderLoop = () => {
-
+    render();
+    requestAnimationFrame(() => {
+      renderLoop();
+    });
   };
+  renderLoop();
 };
