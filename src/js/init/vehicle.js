cf7c4dc6760b01a658d711762941cb21f3a6f898
yoichi kobayashi <info@tplh.net>
2016-08-29 22:57:26 +0900

    [add] moving function for vehicle.

--- src/js/init/vehicle.js
+++ src/js/init/vehicle.js
@@ -5,14 +5,37 @@ const glMatrix = require('gl-matrix');
 export default function() {
   const canvas = document.getElementById('stage');
   const ctx = canvas.getContext('2d');
-  const vehicles = [];
+  const vehicles_v = [];
+  const vehicles_a = [];
 
   let time = 0;
 
+  const init = () => {
+    for (var i = 0; i < 100; i++) {
+      vehicles_v.push(
+        Math.random() * window.innerWidth,
+        Math.random() * window.innerHeight,
+        0
+      );
+      // vehicles_a.push(0, 0, 0);
+      vehicles_a.push(
+        Math.random() * 4 - 2,
+        Math.random() * 4 - 2,
+        0
+      );
+    }
+  };
   const render = () => {
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    ctx.arc(window.innerWidth / 2, window.innerHeight / 2, 100, 0, Math.PI * 2, false);
-    ctx.fill();
+    for (var i = 0; i < vehicles_v.length; i += 3) {
+      vehicles_v[i + 0] += vehicles_a[i + 0];
+      vehicles_v[i + 1] += vehicles_a[i + 1];
+      vehicles_v[i + 2] += vehicles_a[i + 2];
+      ctx.beginPath();
+      ctx.arc(vehicles_v[i + 0], vehicles_v[i + 1], 10, 0, Math.PI * 2, false);
+      ctx.fill();
+      ctx.closePath();
+    }
   };
   const renderLoop = () => {
     render();
@@ -20,5 +43,6 @@ export default function() {
       renderLoop();
     });
   };
+  init();
   renderLoop();
 };
